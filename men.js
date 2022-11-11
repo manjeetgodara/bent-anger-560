a37d8eb87dab2131c57830492409e31b530ad73f
Manjeet <godaramanjeet293@gmail.com>
2022-11-11 19:05:12 +0530

    Today's task done

---
+++ men.js
@@ -0,0 +1,38 @@
+let url="https://636cdef7ab4814f2b270abc9.mockapi.io/mens";
+
+let bag=[];
+fetch(url).then((res)=>res.json()).then((data)=>{
+bag.push(data);
+displayTable(data);
+});
+
+let searchBox=document.querySelector(".search").addEventListener("input",function(){
+    let q=document.querySelector(".searchBox").value;
+   
+  let newData=  bag[0].filter((elem)=>{
+        return elem.brand.toLowerCase().includes(q.toLowerCase());
+    });
+    displayTable(newData);
+})
+
+function displayTable(data){
+    document.querySelector("#container").innerHTML="";
+    data.forEach(element => {
+        let div=document.createElement("div");
+        
+        let image=document.createElement("img");
+          image.setAttribute("src",element.img);
+
+        let price=document.createElement("h3");
+            price.innerText=element.price;
+        let Brand=document.createElement("h5");
+              Brand.innerText=element.brand;
+        let color=document.createElement("p");
+           color.innerText="Color:-"+element.color;
+        let btn=document.createElement("button");
+btn.innerText="Add To Cart";
+        document.querySelector("#container").append(div);
+
+        div.append(image,price,Brand,color,btn);
+    });
+}
